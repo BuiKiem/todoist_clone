e393b7b14506bc8b320b6b233d04b11374297436
Trung Bui <buivantrung1105@gmail.com>
2020-01-30 15:25:03 +0700

    Configure primary and secondary color palette

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,6 +1,16 @@
 import { createMuiTheme, responsiveFontSizes } from "@material-ui/core";
 
-let theme = createMuiTheme({});
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#DB4C3F"
+    },
+    secondary: {
+      main: "#B8B8B8"
+    }
+  }
+});
+
 theme = responsiveFontSizes(theme);
 
 export {theme};
